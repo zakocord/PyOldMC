84563a203456f7f3326757a9c94fd33325912e8b
zakocord <beehive_dev@outlook.jp>
2025-04-30 12:02:12 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import webbrowser
 import time
 
 webbrowser.open('minecraft://')
-time.sleep(3)
+time.sleep(10)
 webbrowser.open('minecraft://openServersTab')
 webbrowser.open("https://discord.gg/VaHm3EQyQZ")
